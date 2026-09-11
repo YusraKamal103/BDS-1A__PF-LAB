69e1cf6d8440131f4639cd40ec93fdcded5e0df3
Yusra Kamal <yusrakamal103@gmail.com>
2026-09-11 19:30:52 +0000

    assignments

---
+++ Lab 4/question 2.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+int main(){
+    
+    int score;
+    printf("Enter your confidence score:  ");
+    scanf("%d",&score);
+    
+    if (score<0 || score>100){
+        printf("Invalid score\n");
+    }
+    else if (score>=0 && score<=49) {
+        printf("Low Confidence\n");
+    }
+    else if (score>=50 && score<=79) {
+        printf("Moderate Confidence\n");
+    }
+    else if (score>=80 && score<=100) {
+        printf("High Confidence\n");
+    }
+
+
+ return 0;
+
+}
